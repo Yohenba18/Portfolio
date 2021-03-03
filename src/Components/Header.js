666263a8aabdb044a7502c95728d6ba85d70c044
Yohenba18 <roddik12kshetri@gmail.com>
2021-03-03 15:29:42 +0530

    portfolio v1.3 - header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,29 +1,30 @@
 import React from "react";
 import "./Header.css";
 
+
 function Header() {
   return (
     <div>
       <div className="header">
         <div className="header__left">
-          <img src="" alt="" />
+          {/* <CodeIcon/>*/}
           <div className="header__title">
-            <h2>Header</h2>
+            <h2>Porfolio</h2>
           </div>
         </div>
 
         <div className="header__right">
           <div className="header__options">
-            <h2>About Me</h2>
+            <h4>About Me</h4>
           </div>
           <div className="header__options">
-            <h2>Projects</h2>
+            <h4>Projects</h4>
           </div>
           <div className="header__options">
-            <h2>Hobbies</h2>
+            <h4>Hobbies</h4>
           </div>
           <div className="header__options">
-            <h2>Contact Me</h2>
+            <h4>Contact Me</h4>
           </div>
         </div>
       </div>
