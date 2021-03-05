e3b06006d42db86a106987c94bb9d5c46de5de52
Yohenba18 <roddik12kshetri@gmail.com>
2021-03-05 14:51:29 +0530

    portfolio v3.3 about-card

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Project.css";
 import ProjectCard from "./ProjectCard";
+import facebook from "../images/facebook.png"
 
 function Project() {
   return (
@@ -9,13 +10,33 @@ function Project() {
         <h1>PROJECTS</h1>
       </div>
       <div className="project__cards">
-        <ProjectCard title="Portfolio" />
-        <ProjectCard title="Portfolio"/>
-        <ProjectCard title="Portfolio"/>
-        <ProjectCard title="Portfolio"/>
-        <ProjectCard title="Portfolio"/>
-        <ProjectCard title="Portfolio"/>
+        <ProjectCard
+          title="Facebook clone"
+          description="Guided projects to practice Frontend devlopment and backend with Firebase and Firestore with authentication"
+          link="https://github.com/Yohenba18/fb-react.github.io"
+          image={facebook}
+        />
+        <ProjectCard
+          title="LinkedIn clone"
+          description="Guided projects to practice Frontend devlopment and backend with Firebase and Firestore"
+          link="https://github.com/Yohenba18/LinkedIn-clone"
 
+        />
+        <ProjectCard
+          title="Twitter clone"
+          description="Guided projects to practice Frontend devlopment and backend with Firebase and Firestore"
+          link="https://github.com/Yohenba18/twitter-clone"
+        />
+        <ProjectCard
+          title="Instagram clone"
+          description="Guided projects to practice Frontend devlopment and backend with Firebase and Firestore"
+          link="https://github.com/Yohenba18/instagram-clone"
+        />
+        <ProjectCard
+          title="Portfolio"
+          description="My basic beginner Portfolio made with HTML, CSS and JAVASCRIPT"
+          link="https://github.com/Yohenba18/Portfolio-uncomplete"
+        />
       </div>
     </div>
   );
