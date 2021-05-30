8655270eef66b4a5b091a3980c53ec8e5614b2ba
Yohenba18 <roddik12kshetri@gmail.com>
2021-05-30 11:46:31 +0530

    experience updated

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,6 +10,7 @@ function Experience() {
             <div className="experience__right">
                 <h3>1. Web Developer in <b style={{color: "#D88D0E"}}>IEEE SRMIST STUDENT BRANCH</b></h3>
                 <h3>2. Blockchain domain member in <b style={{color: "#D88D0E"}}>DS COMMUNITY SRM</b></h3>
+                <h3>2. Blockchain Associate in <b style={{color: "#D88D0E"}}>NextTechLab</b></h3>
                 <h3>3. Cerficates on <b style={{color: "#D88D0E"}}>Fullstack Development</b> by Coursera</h3>
             </div>
         </div>
